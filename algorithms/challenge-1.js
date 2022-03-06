fffec3fc3803329dbd2fe31fb67400d7ecee744b
Nwankwo Ozioma <officialozioma@gmail.com>
2022-03-06 12:54:58 +0100

    First Phase 2nd Assignment for Decagon

--- algorithms/challenge-1.js
+++ algorithms/challenge-1.js
@@ -1,3 +1,25 @@
-function missingNumber(numArr) {}
+function missingNumber(numArr = []) {
+  var arrMising = true;
+  var arrMiss = [];
+  var b = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  arrMising = b.filter(function (missing) {
+    if (!numArr.includes(missing)) {
+      arrMiss.push(missing);
+    }
+    return arrMiss;
+  });
+  if (arrMiss.length === 0) {
+    arrMising = false;
+    console.log(arrMising);
+    arrMiss = [];
+    return;
+  }
+  console.log(arrMiss);
+arrMiss.sort(function(a, b){return a-b});
+  console.log(arrMiss);
 
-module.exports = missingNumber
+
+  arrMiss = [];
+}
+
+module.exports = missingNumber;
--- algorithms/challenge-2.js
+++ algorithms/challenge-2.js
@@ -1,3 +1,20 @@
-function numSwap(number) {}
+function numSwap(number){
+  if (number.toString().length % 2 != 0) {
+    return false;
+  }
+  var swappedResult=0;
+  var addNumb1 =1;
+  while(number !=0){
+
+    var nomber1 = number % 10;
+        var nomber2 = (number- nomber1) / 10 % 10;
+    swappedResult += addNumb1 * (10 * nomber1 + nomber2);
+  
+    number = Math.floor(number / 100);
+
+    addNumb1 *= 100;
+  }
+    return swappedResult;
+}
 
 module.exports = numSwap
