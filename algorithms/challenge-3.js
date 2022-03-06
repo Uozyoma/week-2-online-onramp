d572b69b5e9ce69a547e0c7dc132832e70be57d1
Nwankwo Ozioma <officialozioma@gmail.com>
2022-03-06 22:03:24 +0100

    Decagon Assignment

--- algorithms/challenge-3.js
+++ algorithms/challenge-3.js
@@ -1,15 +1,15 @@
 function increment(arr, value) {
 
 console.log(arr);
-var i;
+var j;
 var obj2 =Object.keys(arr);
 
 var me= arr[0].val;
 
 var arty={};
-for(i=0; i<obj2.length; i++){
+for(j=0; j<obj2.length; j++){
 
-  arr[i].val = arr[i].val + value;
+  arr[j].val = arr[j].val + value;
   console.log(arr);
 
 }
