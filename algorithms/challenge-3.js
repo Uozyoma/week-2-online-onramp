a14e0bebce3ebe47b44ba22346eff2774d6d13b2
Nwankwo Ozioma <officialozioma@gmail.com>
2022-03-06 17:41:54 +0100

    Updated Decadev Assignment

--- algorithms/challenge-3.js
+++ algorithms/challenge-3.js
@@ -1,3 +1,20 @@
-function increment(arr, value) {}
+function increment(arr, value) {
 
+console.log(arr);
+var i;
+var obj2 =Object.keys(arr);
+
+var me= arr[0].val;
+
+var arty={};
+for(i=0; i<obj2.length; i++){
+
+  arr[i].val = arr[i].val + value;
+  console.log(arr);
+
+}
+
+
+
+}
 module.exports = increment
--- algorithms/challenge-4.js
+++ algorithms/challenge-4.js
@@ -1,3 +1,27 @@
-function withdraw(amount) {}
+function withdraw(amount){
+  var note1=100;
+  var note2=50;
+  var note3=20;
+  var mindNote;
+  var mineNote;
+
+  var withdrawable =[];
+
+  if((amount >=40) &&(amount <=10000) ){
+   note1=parseInt(amount/100);
+   withdrawable.push(note1);
+  mindNote = amount -(note1 * 100);
+  note2= parseInt(mindNote/50);
+   withdrawable.push(note2);
+mineNote = mindNote -(note2 * 50);
+console.log(mineNote);
+  note3=parseInt(mineNote/20);
+   withdrawable.push(note3);
+   console.log(withdrawable);
+
+  }
+}
+
+
 
 module.exports = withdraw
